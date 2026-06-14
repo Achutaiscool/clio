1d76f9d91f13ada25fd015626ced35467e7f6493
Achutaiscool <achuta707@gmail.com>
2026-06-14 20:56:23 +0530

    feat: implement note save, display, and delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 import { useState } from "react"
 export default function App(){
     const[text,setText]=useState()
+    const[notesArray,setNotesArray]=useState([])
+    const[delInput,setDel]=useState()
+    const[count,setCount]=useState(1)
+
     return(
         <>
         <textarea name="note" 
         value={text}
-        onChange={(e)=>{setText(e.target.value)}}
+        onChange={(e)=>{setText(e.target.value)
+        }}
         />
-        <button>save</button>
-        <p>{text}</p>
+        <button onClick={()=>{
+             const newNote = {
+                id: count,
+                note: text,
+                time: new Date().toISOString()
+            }
+            setNotesArray([...notesArray, newNote])
+            setCount(count+1)
+            setText("")
+        }}>save</button>
+        <br />
+        <br />
+        <ul>
+        {
+            notesArray.map((notes)=>{
+                return <li key={notes.id}>{notes.note}
+                    <button onClick={()=>{
+                        setNotesArray(notesArray.filter((n)=>n.id!==notes.id))
+                    }}>
+                        delete
+                    </button>
+                </li>
+            })
+        }
+        </ul>
         </>
     )
 }
