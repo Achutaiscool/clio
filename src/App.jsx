11e911bbc0a2fede26cebba696947fcec5da30d1
Achutaiscool <achuta707@gmail.com>
2026-06-20 16:29:05 +0530

    feat:localStorage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import { useState } from "react"
+import { useEffect } from "react"
+import { useRef } from "react"
 export default function App(){
     const[text,setText]=useState()
     const[notesArray,setNotesArray]=useState([])
-    const[delInput,setDel]=useState()
     const[count,setCount]=useState(1)
+    const isFirstRender = useRef(true)
+    useEffect(()=>{
+        const result=localStorage.getItem('notes')
+            console.log('Loading from localStorage:', result)
 
+        if (result){
+            setNotesArray(JSON.parse(result))
+        }
+    },[])
+    useEffect(()=>{
+        if (isFirstRender.current) {
+        isFirstRender.current = false
+        return
+    }
+        localStorage.setItem('notes',JSON.stringify(notesArray))
+    },[notesArray]
+    )
+    console.log(localStorage.getItem('notes'))
     return(
         <>
         <textarea name="note" 
@@ -21,18 +39,19 @@ export default function App(){
             setNotesArray([...notesArray, newNote])
             setCount(count+1)
             setText("")
+            
         }}>save</button>
         <br />
         <br />
         <ul>
         {
-            notesArray.map((notes)=>{
-                return <li key={notes.id}>{notes.note}
-                    <button onClick={()=>{
-                        setNotesArray(notesArray.filter((n)=>n.id!==notes.id))
-                    }}>
-                        delete
-                    </button>
+        notesArray.map((notes)=>{
+            return <li key={notes.id}>{notes.note}
+                <button onClick={()=>{
+                    setNotesArray(notesArray.filter((n)=>n.id!==notes.id))
+                }}>
+                delete
+                </button>
                 </li>
             })
         }
